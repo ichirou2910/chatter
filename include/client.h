5a7499ecbbfa51a6b4b543c3a7afd8dddb0b3d89
nor304 <phanbac3042000@gmail.com>
2020-12-12 08:34:03 +0700

    Client initial commit

---
+++ include/client.h
@@ -0,0 +1,20 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 9000
+
+#define MAX_CLIENTS 100
+#define BUFFER_SZ 1024
+#define NAME_LEN 32
+#define GROUP_ID_LEN 7
+#define PASSWORD_LEN 32
+
+void str_overwrite_stdout();
+void str_trim_lf(char* arr, int length);
+void catch_ctrl_c_and_exit();
+void print_msg(char* str, int mode);
+void recv_msg_handler();
+void send_msg_handler();
+
+#endif
---
+++ src/client.c
@@ -0,0 +1,163 @@
+#include "client.h"
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <time.h>
+
+volatile sig_atomic_t flag = 0;
+int sockfd = 0;
+char name[NAME_LEN];
+time_t now;
+struct tm* local;
+
+void str_overwrite_stdout() {
+    printf("\r%s", "> ");
+    fflush(stdout);
+}
+
+void str_trim_lf(char* arr, int length) {
+    for (int i = 0; i < length; i++) {
+        if (arr[i] == '\n') {
+            arr[i] = '\0';
+            break;
+        }
+    }
+}
+
+void catch_ctrl_c_and_exit() {
+    flag = 1;
+}
+
+void recv_msg_handler() {
+    char message[BUFFER_SZ] = {};
+
+    while (1) {
+        int receive = recv(sockfd, message, BUFFER_SZ, 0);
+
+        if (receive > 0) {
+            time(&now);
+            local = localtime(&now);
+            printf("%02d:%02d ~ %s\n", local->tm_hour, local->tm_min, message);
+            str_overwrite_stdout();
+        }
+        else if (receive == 0) {
+            break;
+        }
+        bzero(message, BUFFER_SZ);
+    }
+}
+
+void send_msg_handler() {
+    char buffer[BUFFER_SZ] = {};
+
+    while (1) {
+        str_overwrite_stdout();
+        fgets(buffer, BUFFER_SZ, stdin);
+        str_trim_lf(buffer, BUFFER_SZ);
+
+        if (!strcmp(buffer, ":help") || !strcmp(buffer, ":h")) {
+            printf("Chatter commands:\n");
+            printf("- :c | :create <password>     - Create a new room with <password>\n");
+            printf("- :j | :join <id> <password>  - Join a room with <id> & <password>\n");
+            printf("- :s | :switch <id>           - Switch to room with <id>\n");
+            printf("- :l | :leave                 - Temporary leave room and return to lobby\n");
+            printf("- :r | :rename <name>         - Rename self to <name>\n");
+            printf("- :q | :quit                  - Quit current room and return to lobby\n");
+            printf("- :f | :file <filename>       - Send file with <filename> to roommate\n");
+            printf("- :i | :info                  - Print room info\n");
+            printf("===\n");
+            printf("Press Ctrl+C to quit Chatter\n");
+        }
+        else {
+            time(&now);
+            local = localtime(&now);
+            send(sockfd, buffer, strlen(buffer), 0);
+        }
+        bzero(buffer, BUFFER_SZ);
+    }
+    catch_ctrl_c_and_exit();
+}
+
+int main() {
+    const char* ip = SERVER_IP;
+    int port = PORT;
+    // if (argc < 2) {
+    //     printf("Usage: %s password [group_id]\n", argv[0]);
+    //     printf("If provided group_id, join room with password and group_id\n");
+    //     printf("If not provided group_id, create new room with password\n");
+    //     return EXIT_FAILURE;
+    // }
+
+    time(&now);
+    local = localtime(&now);
+
+    signal(SIGINT, catch_ctrl_c_and_exit);
+
+    printf("Enter your name: ");
+    fgets(name, NAME_LEN, stdin);
+    str_trim_lf(name, strlen(name));
+
+    if (strlen(name) > NAME_LEN || strlen(name) < 2) {
+        printf("ERROR: Enter name correctly\n");
+        return EXIT_FAILURE;
+    }
+    else if (!strcmp(name, "SYSTEM")) {
+        printf("ERROR: Reserved name. Please use another");
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in serv_addr;
+
+    // Socket Settings
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(ip);
+    serv_addr.sin_port = htons(port);
+
+    // Connect to the server
+    int err = connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (err == -1) {
+        printf("ERROR: connect\n");
+        return EXIT_FAILURE;
+    }
+
+    send(sockfd, name, NAME_LEN, 0);
+
+    printf("=== WELCOME TO CHATTER ===\n");
+    printf("Current time: %s", ctime(&now));
+    printf("Type :h or :help for Chatter commands\n");
+
+    // Thread for sending the messages
+    pthread_t send_msg_thread;
+    if (pthread_create(&send_msg_thread, NULL, (void*)send_msg_handler, NULL) != 0) {
+        printf("ERROR: pthread\n");
+        return EXIT_FAILURE;
+    }
+
+    // Thread for receiving messages
+    pthread_t recv_msg_thread;
+    if (pthread_create(&recv_msg_thread, NULL, (void*)recv_msg_handler, NULL) != 0) {
+        printf("ERROR: pthread\n");
+        return EXIT_FAILURE;
+    }
+
+    while (1) {
+        if (flag) {
+            printf("\nGoodbye\n");
+            break;
+        }
+    }
+
+    close(sockfd);
+
+    return EXIT_SUCCESS;
+}
